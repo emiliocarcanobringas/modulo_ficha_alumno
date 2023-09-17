95f044cf6f114704b4accd83b1198dc980a00a65
Emilio Carcaño Bringas <emiliocarcano@uninaciones.edu.mx>
2023-09-16 20:39:42 -0400

    Update capturar_alumno.php

--- capturar_alumno.php
+++ capturar_alumno.php
@@ -3,40 +3,34 @@
 <head>
     <title>Capturar Alumno</title>
     <link rel="stylesheet" href="capturar_alumno.css">
-    <script>
-        function updateWhatsAppLength() {
-            let input = document.getElementById('whatsapp-input');
-            let feedback = document.getElementById('whatsapp-feedback');
-            let length = input.value.length;
-
-            if (length < 10) {
-                feedback.textContent = `Faltan ${10 - length} números.`;
-            } else {
-                feedback.textContent = '';
-            }
-        }
-    </script>
 </head>
 <body>
-    <form action="guardar_alumno.php" method="post" class="form-plan">
+    <form action="guardar_alumno.php" method="post" class="form-plan" autocomplete="off">
+        <label for="matricula">Matricula:</label>
+        <input type="text" id="matricula" name="matricula" maxlength="20" required autocomplete="off">
+
         <label for="nombre">Nombre:</label>
-        <input type="text" id="nombre" name="nombre" required>
+        <input type="text" id="nombre" name="nombre" maxlength="100" required autocomplete="off">
 
         <label for="apellido_paterno">Apellido Paterno:</label>
-        <input type="text" id="apellido_paterno" name="apellido_paterno" required>
+        <input type="text" id="apellido_paterno" name="apellido_paterno" maxlength="100" required autocomplete="off">
 
         <label for="apellido_materno">Apellido Materno:</label>
-        <input type="text" id="apellido_materno" name="apellido_materno" required>
+        <input type="text" id="apellido_materno" name="apellido_materno" maxlength="100" autocomplete="off">
+
+        <label for="grupo">Grupo:</label>
+        <input type="text" id="grupo" name="grupo" maxlength="10" required autocomplete="off">
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" maxlength="150" autocomplete="off">
+
+        <label for="celular">Celular:</label>
+        <input type="text" id="celular" name="celular" maxlength="20" autocomplete="off">
 
         <label for="fecha_de_nacimiento">Fecha de Nacimiento:</label>
-        <input type="date" id="fecha_de_nacimiento" name="fecha_de_nacimiento" pattern="\d{4}-\d{2}-\d{2}" required>
-
-        <label for="WhatsApp">WhatsApp:</label>
-        <input type="text" id="whatsapp-input" name="WhatsApp" maxlength="10" onkeyup="updateWhatsAppLength()" required>
-        <span id="whatsapp-feedback" class="feedback"></span>
+        <input type="date" id="fecha_de_nacimiento" name="fecha_de_nacimiento" pattern="\d{4}-\d{2}-\d{2}" autocomplete="off">
 
         <input type="submit" value="Guardar">
     </form>
 </body>
 </html>
-
